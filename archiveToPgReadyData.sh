a8a675317ff3f8ab7103fe07cc294f562f648d10
Romain Cambonie <romain.cambonie@gmail.com>
2023-03-19 08:55:08 +0100

    feat: added client reference in drives

--- archiveToPgReadyData.sh
+++ archiveToPgReadyData.sh
@@ -4,7 +4,7 @@ echo "Starting data extraction on archive" $1
 filename=$1
 filename_without_extension=${filename//.gz/}
 
-entities=(addresses fares drives plannings users drivers has_fare drive_from drive_to)
+entities=(addresses fares drives plannings users drivers has_fare drive_from drive_to has_drive)
 
 createTableFromCsvHeaders() {
   headers=$1
@@ -75,10 +75,16 @@ jq 'map(select(has("@class")))' records > class
  jq 'map(select(."@class" | contains("User")))' class > users
  jq 'map(select(."@class" | contains("Driver")))' class > drivers
 
- # Extraction of edge class
+ # Extraction of edge classes
  jq 'map(select(."@class" | contains("has_fare")))' class > has_fare #drive_and_fare
  jq 'map(select(."@class" | contains("drive_from")))' class > drive_from #drive_and_address
  jq 'map(select(."@class" | contains("drive_to")))' class > drive_to #drive_and_address
+
+
+ jq 'map(select(."@class" | contains("has_drive")))' class > has_drive #client_and_drive
+
+###
+#psql "postgresql://postgres:password@localhost:5432/taxi" -c "DROP VIEW IF EXISTS view_result;"
 
 ### Transform jq records into csv
 for entityName in "${entities[@]}"; do
@@ -86,7 +92,7 @@ for entityName in "${entities[@]}"; do
 done
 
 # Migrations
-psql "postgresql://postgres:password@localhost:5432/taxi" -c "\i ../migrations/fares_drive_rid_from_has_fare.sql"
-psql "postgresql://postgres:password@localhost:5432/taxi" -c "\i ../migrations/drive_from_to_address_values.sql"
-
-# Dropping edge tables
+psql "postgresql://postgres:password@localhost:5432/taxi" -c "\i ../migrations/1_fares_drive_rid_from_has_fare.sql"
+psql "postgresql://postgres:password@localhost:5432/taxi" -c "\i ../migrations/2_drive_from_to_address_values.sql"
+psql "postgresql://postgres:password@localhost:5432/taxi" -c "\i ../migrations/3_drive_client_rid_from_has_drive.sql"
+#psql "postgresql://postgres:password@localhost:5432/taxi" -c "\i ../migrations/view_result.sql"
